3975c24cbe72441b5bb6609f941a71c69429f1d6
YahirSolar <yahirsolar1@gmail.com>
2023-03-07 23:40:35 -0600

    Clase Concesionaria

--- Concesionaria.cs
+++ Concesionaria.cs
@@ -15,40 +15,41 @@ class Concesionaria {
             Autos.Add(auto);
             actual += 1;
         }else{
-            Console.WriteLine("No hay mas espacio en la Concesionaria");
+            Console.WriteLine("\nNo hay mas espacio en la Concesionaria\n");
         }
     }
     public void MostrarAuto(Automovil auto) {
         if(auto != null) {
             foreach(Automovil item in Autos) {
-                if(item == auto){
+                if(item == auto) {
                     Console.WriteLine(item);
                     return; 
                 }
             }
-        Console.WriteLine("El auto que buscas no está en la Concesionario");
+        Console.WriteLine("\nEl auto que buscas no está en el Concesionario\n");
         }
     }
     public void EliminarAuto(Automovil auto) {
         if(actual != 0){
             foreach(Automovil item in Autos){
-                if(item == auto){
+                if(item == auto) {
                     Autos.Remove(item);
                     actual -= 1;
                     return; 
                 }
             }
         }else{
-            Console.WriteLine("No hay autos que eliminar de la Concesionaria");
+            Console.WriteLine("\nNo hay autos que eliminar de la Concesionaria\n");
         }
     }
     public void MostrarAutos() {
+        Console.WriteLine("\nAutos de la Concecionaria SamuPelonSA\n");
         foreach(Automovil item in Autos){
             Console.WriteLine(item);
         }
     }
     public void VaciarConcesionario(Automovil auto) {
         Autos.Clear();
-        Console.WriteLine("La Concesionaria está vacia");
+        Console.WriteLine("\nLa Concesionaria está vacia se los robo el samu pelon\n");
     }
 }
